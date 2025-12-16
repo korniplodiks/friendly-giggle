a4f56e7414868fde0b72f9413d139a64f88fa3c4
korniplodiks <korniplodiks@users.noreply.github.com>
2025-12-16 02:20:11 +0200

    test: performance optimization

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 19
 module.exports = API;
+
+
+// API module for GiggleChat
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 22
+module.exports = API;
